9747a1ad9d56d27d85435465f86c17ebbf1a5dcf
fukuhara-ka <peregrinorum0129@yahoo.co.jp.>
2020-03-27 10:03:12 +0900

    課題提出 - Javaの基礎

---
+++ basic/src/basic/Task1.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package basic;
+
+/**
+ * @author fukuhara
+ *
+ */
+public class Task1 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // 出力
+        System.out.println("初めての課題");
+        System.out.print("Java攻略の道");
+
+    }
+
+}
